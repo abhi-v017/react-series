8ed697ed7027791ae3b9de23a0537013f35d3668
abhishek verma <abhi.av017@gmail.com>
2024-10-08 19:34:17 +0530

    remaining part of 10

--- 10/src/Components/TodoItem.jsx
+++ 10/src/Components/TodoItem.jsx
@@ -36,7 +36,6 @@ function TodoItem({todo}) {
                 className="inline-flex w-8 h-8 rounded-lg text-sm border border-black/10 justify-center items-center bg-gray-50 hover:bg-gray-100 shrink-0 disabled:opacity-50"
                 onClick={() => {
                     if (todo.completed) return;
-
                     if (isTodoEditable) {
                         editTodo();
                     } else setIsTodoEditable((prev) => !prev);
@@ -45,7 +44,7 @@ function TodoItem({todo}) {
             >
                 {isTodoEditable ? "📁" : "✏️"}
             </button>
-            {/* Delete Todo Button */}
+            {/*Delete Todo Button*/}
             <button
                 className="inline-flex w-8 h-8 rounded-lg text-sm border border-black/10 justify-center items-center bg-gray-50 hover:bg-gray-100 shrink-0"
                 onClick={() => deleteTodo(todo.id)}
