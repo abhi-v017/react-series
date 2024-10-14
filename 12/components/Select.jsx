4bf6d6e084e8226d7758e6f6025dd949e4f50c52
abhishek verma <abhi.av017@gmail.com>
2024-10-14 12:39:21 +0530

    component's css

--- 12/components/Select.jsx
+++ 12/components/Select.jsx
@@ -1,11 +1,29 @@
-import React from 'react'
+import React, { useId } from 'react'
 
-function Select() {
+function Select({
+  options,
+  label,
+  classname,
+  ...props
+}, ref) {
+  const id = useId()
   return (
     <div>
-      
+      {label && <label htmlFor={id} className=''></label>}
+      <select
+        id={id}
+        {...props}
+        className={`px-3 py-2 rounded-lg bg-white text-black outline-none focus:bg-gray-50 duration-200 border border-gray-200 w-full ${classname}`}
+        ref={ref}
+      >
+          {options?.map((option)=>{
+            <option key={option} value={option}>
+              {option}
+            </option>
+          })}
+      </select>
     </div>
   )
 }
 
-export default Select
+export default React.forwardRef(Select)
